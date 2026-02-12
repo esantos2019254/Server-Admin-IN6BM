ef39d6970f004aee7cba28201365f1e9b691904d
Elmer Santos <esantos2019254@kinal.edu.gt>
2026-02-12 11:31:39 -0600

    cloudinary

--- src/fields/field.routes.js
+++ src/fields/field.routes.js
@@ -1,10 +1,14 @@
 import { Router } from 'express';
 import { createField, getFields } from './field.controller.js';
+import { uploadFieldImage } from '../../middlewares/file-uploader.js';
+import { cleanUploaderFileOnFinish } from '../../middlewares/delete-file-on-error.js';
 
 const router = Router();
 
 router.post(
     '/create',
+    uploadFieldImage.single('image'),
+    cleanUploaderFileOnFinish,
     createField
 )
 
